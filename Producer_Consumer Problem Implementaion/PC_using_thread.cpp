6bdd562bc545a783bd61cd6f9ca63969a1760cee
Aditya <kumawataditya1358@gmail.com>
2021-02-22 19:08:42 +0530

    commenting in the code and improve slightly bug

--- Producer_Consumer Problem Implementaion/PC_using_thread.cpp
+++ Producer_Consumer Problem Implementaion/PC_using_thread.cpp
@@ -7,42 +7,70 @@
 
 using namespace std;
 
-mutex m;
-condition_variable cv;
-deque<int> productBuffer;
-const int maxBufferSize = 10;
-
+//declare variable
+mutex m, lockPthread; // mutex to get mutual exclusion
+condition_variable cv; //condition variable to get the functionality of notify_one and notify_all
+deque<int> productBuffer; // Buffer as a queue
+const int maxBufferSize = 10; // Buffer size of products
+int producerThread ; // total number of thread that all will notify by incrementing this variable to prevent consumer for more wait
+int produced ; // variable to prevent the consumer to wait more if there will be no production in future
 
 //Producer producing 
 void producer(int product){
     while(product){
+        //using unique_lock method to acquire the mutex
+        //crtical section Entry
         unique_lock<mutex> locker(m);
-        cv.wait(locker,[](){return productBuffer.size()<maxBufferSize;});
+        cv.wait(locker,[](){return productBuffer.size()<maxBufferSize;}); //checking condition if production is possible or not
 
-        cout<<"Producer is producing Product #: "<<product<<" ......."<<endl;
-        sleep(1);
+        cout<<"Producer is producing Product #: "<<product<<" ....."<<endl;
+        // sleep(1);
+
         productBuffer.push_back(product);
         product--;
 
         locker.unlock();
         cv.notify_one();
+        //crtical section Exit
     }
+    
+    // critical section Entry (It handle the case of more than one thread of producer)
+    lockPthread.lock();
+    produced++;
+    lockPthread.unlock();
+    //critical sectino Exit
+
     return ;
 }
 
 
 //Consumer consuming
 void consumer(){
+    bool flag = false;
     while(1){
+        //using unique_lock method to acquire the mutex
+        //crtical section Entry
         unique_lock<mutex> locker(m);
-        cv.wait(locker,[](){return productBuffer.size()>0;});
+        
+        //check condition when no product reamaing in buffer and all producer has been produced , in future no production 
+        if(producerThread==produced && productBuffer.size()==0){
+            flag = true;
+            goto label ;
+        }
+        cv.wait(locker,[](){return productBuffer.size()>0;}); //checking condition if Consumption is possible or not
 
-        cout<<"Consumer is consuming Product #: "<<productBuffer.front()<<" ...."<<endl;
-        sleep(1);
+        cout<<"Consumer is consuming Product #: "<<productBuffer.front()<<" ....."<<endl;
+        // sleep(1);
         productBuffer.pop_front();
 
+        label :
         locker.unlock();
+        if(flag){
+            break; // break the loop if noproduction in future
+        }
+
         cv.notify_one();
+        //critical section Exit
     }
     return ;
 }
@@ -50,8 +78,13 @@ void consumer(){
 
 int main(int argc, char* argv[]){
     
+    //limit the product number
     int product = 50;
-    thread t1(consumer);
+    producerThread = 1;
+    produced = 0;
+
+    // create the thread
+    thread t1(consumer);    
     thread t2(producer, product);
 
     t1.join();
